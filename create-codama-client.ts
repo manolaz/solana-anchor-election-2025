6e2439097a2a8d790d03116c60d92e7d891eaef7
Mike MacCana <mike.maccana@gmail.com>
2025-05-28 21:30:30 -0400

    Formatting only

--- create-codama-client.ts
+++ create-codama-client.ts
@@ -9,20 +9,20 @@ import { promises as fs } from "fs";
 const loadAnchorIDL = async () => {
   const basePath = path.join("target", "idl");
   const dirPath = path.join(basePath);
-  
+
   try {
     // Read the directory contents
     const files = await fs.readdir(dirPath);
     const jsonFiles = files.filter(file => file.endsWith('.json'));
-    
+
     if (jsonFiles.length === 0) {
       throw new Error(`No JSON files found in ${dirPath}`);
     }
-    
+
     if (jsonFiles.length > 1) {
       throw new Error(`Multiple JSON files found in ${dirPath}. Please specify which one to use.`);
     }
-    
+
     const filePath = path.join(dirPath, jsonFiles[0]);
     return JSON.parse(await fs.readFile(filePath, "utf-8"));
   } catch (error) {
